7e6dfc635c0e5a4b258965f2996be3fde44e8718
Dean de Haast <Dean de Haast>
2016-03-23 16:37:42 +0200

    Target Movement is working

---
+++ Assets/Scripts/player_target.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_target : Entity {
+	public float walkingSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		PlayerMovement ();
+	}
+
+	void PlayerMovement(){
+		if (Input.GetKey(KeyCode.D))
+		{
+			transform.position += new Vector3(walkingSpeed * Time.deltaTime, 0.0f, 0.0f);
+			transform.eulerAngles = new Vector3 (0, 0, 0);
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			transform.position -= new Vector3(walkingSpeed * Time.deltaTime, 0.0f, 0.0f);
+			transform.eulerAngles = new Vector3 (0, 0, 180);
+		}
+		if (Input.GetKey(KeyCode.W))
+		{
+			transform.position += new Vector3(0.0f, walkingSpeed * Time.deltaTime, 0.0f);
+			transform.eulerAngles = new Vector3 (0, 0, 90);
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			transform.position -= new Vector3(0.0f, walkingSpeed * Time.deltaTime, 0.0f);
+			transform.eulerAngles = new Vector3 (0, 0, 270);
+		}
+	}
+}
