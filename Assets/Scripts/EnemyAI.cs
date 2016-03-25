b3b6b4ccb7d156b5abc84393d8c651ca2c6d354f
Dean de Haast <Dean de Haast>
2016-03-25 16:48:59 +0200

    AI turns to face the player.

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -2,13 +2,24 @@
 using System.Collections;
 
 public class EnemyAI : MonoBehaviour {
+	Transform player1;
+
 	public Transform sighStart;
 	public Transform sightEnd;
 	public bool spotted = false;
 	public bool spottedObject = false;
+
+	public GameObject alert;
 	// Use this for initialization
 	void Start () {
 		Debug.Log ("started");
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Raycasting ();
+		Behaviours ();
+		Chase();
 	}
 
 	//Check for line of sight.
@@ -22,10 +33,35 @@ public class EnemyAI : MonoBehaviour {
 	}
 
 	void Behaviours(){
+		if (spotted) {
+			alert.SetActive (true);
+			//Chase();
+		} else{
+			alert.SetActive (false);
+
+		}	
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		Raycasting ();
+
+
+	void Chase(){
+		GameObject go;
+		if (player1 == null) {
+			go = GameObject.Find ("Assassin");
+
+			if(go !=null){
+				player1 = go.transform;
+			}
+		}
+
+		if (player1 == null)
+			return; // Try again next frame if it doesn't exist.
+
+		Vector3 dir = player1.position - transform.position;
+		dir.Normalize ();
+
+		float zAngle = Mathf.Atan2 (dir.y, dir.x) *Mathf.Rad2Deg - 90; //Gets the angle along x axis therefore turn 90
+
+		transform.rotation = Quaternion.Euler (0, 0, zAngle);
 	}
 }
