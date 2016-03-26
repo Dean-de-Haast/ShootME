ce835f404bfc968d5a605ba3b046b379f7bac900
Dean de Haast <Dean de Haast>
2016-03-26 11:34:10 +0200

    Explosion damage doesn't work with particle system in 2d, but changed it so that only explosive barrels can be moved by the red character.

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -2,27 +2,37 @@
 using System.Collections;
 
 public class Explosion : MonoBehaviour {
-
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 	void OnCollisionEnter2D(Collision2D col) {
-		Debug.Log ("WTF");
 		if (col.gameObject.tag == "Bullet") {
 			Destroy (col.gameObject);
 			Debug.Log ("blow up");
+			Explode ();
 		}
 
-		if (col.gameObject.tag == "Player2") {
-
+		if (col.gameObject.tag == "Player2") {	
+			gameObject.GetComponent<Rigidbody2D> ().isKinematic = false;
 		}
+	}
+
+	void OnCollisionExit2D () {
+		gameObject.GetComponent<Rigidbody2D> ().isKinematic = true;
+	}
+
+	void Explode() {
+		Debug.Log ("BLOW 2");
+		var exp = GetComponent<ParticleSystem>();
+		exp.Play();
+		Destroy(gameObject,exp.duration);
 	}
 	
 } 
