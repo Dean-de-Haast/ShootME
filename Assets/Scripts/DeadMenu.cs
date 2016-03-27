4169c3a0e6dd6c8359201baf5e4ddb5afa8d5bda
Dean de Haast <Dean de Haast>
2016-03-27 22:11:17 +0200

    Added in other audio clips

---
+++ Assets/Scripts/DeadMenu.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class DeadMenu : MonoBehaviour {
+	public string mainMenu;
+	public string levelSelect;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void SelectLevel(){
+		SceneManager.LoadScene(levelSelect);
+	}
+
+	public void MainMenu(){
+		SceneManager.LoadScene(mainMenu);
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+	public string level1, level2;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void levelOne(){
+		SceneManager.LoadScene(level1);
+	}
+
+	public void levelTwo(){
+		SceneManager.LoadScene(level2);
+	}
+
+	public void QuitGame(){
+		Application.Quit ();
+	}
+}
